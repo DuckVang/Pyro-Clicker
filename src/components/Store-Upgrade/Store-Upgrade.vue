138de47924be844f93cd29bd29769c6ba9289bf1
Nygen <nducminh002@gmail.com>
2023-02-22 20:05:53 +0100

    Delete eslint and add basic styles to store upgrade

--- src/components/Store-Upgrade/Store-Upgrade.vue
+++ src/components/Store-Upgrade/Store-Upgrade.vue
@@ -15,14 +15,16 @@ export default {
         <Upgerade></Upgerade>
     </div>
 </template>
-<style scoped>
+<style scoped lang="scss">
 .store-upgrade {
+   
     display: flex;
     flex-direction: column;
     background-color: $color1;
     height: 100vh;
     width: 100%;
-    overflow: auto
-    ;
+    overflow: scroll;
+
+
 }
 </style>
--- src/components/Store-Upgrade/Upgrade.vue
+++ src/components/Store-Upgrade/Upgrade.vue
@@ -12,31 +12,40 @@ export default {
 </script>
 <template>
     <div class="upgrade">
-        <div class="icon">
-            >]
-        </div>
+        <div class="icon">>]</div>
         <div class="text">
             Upgrade
         </div>
     </div>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
 .upgrade {
+    
+    margin-top: 10px;
+    padding: 14px;
     display: flex;
     flex-direction: row;
     width: 100%;
-    height: 5vh;
-    background-color: $color1;
+    height: 8vh;
+    background-color: darken($color1, 10);
     border-radius: 1px solid black;
+    align-items: center;
 }
 
 .icon {
     flex: 1;
+    aspect-ratio: 1/1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px dashed black;
 }
 
 .text {
+    flex: 7;
+    font-size: 1.5em;
+    margin-left: 20px;
 
-    flex: 3;
 }
 </style>
