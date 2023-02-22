7baa627c0e54de64905c51493fcd44a0448364c6
Nygen <nducminh002@gmail.com>
2023-02-22 14:33:21 +0100

    Fix button import

---
+++ src/components/Clicker-Button.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+
+</script>
+<template>
+    <button>
+        <p>Click</p>
+    </button>
+</template>
+<style scoped>
+button {
+    width: 100%;
+    max-height: 30%;
+    background-color: red;
+    padding: 5px;
+}
+</style>
